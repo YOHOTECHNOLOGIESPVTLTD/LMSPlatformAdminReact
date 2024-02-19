c130b2acb3844137f8e9cecc39b2c96d41c88d9d
sanjayyoho <140371314+sanjayyoho@users.noreply.github.com>
2024-02-19 15:03:37 +0530

    institute management commit

--- src/views/institute-management/institutes/institutes/ValidatedStepper.js
+++ src/views/institute-management/institutes/institutes/ValidatedStepper.js
@@ -1,38 +1,36 @@
 // ** React Imports
-import { Fragment, useState, forwardRef } from 'react';
+import { Fragment, forwardRef, useState } from 'react';
 
 // ** MUI Imports
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import Step from '@mui/material/Step';
-import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import Step from '@mui/material/Step';
 import Stepper from '@mui/material/Stepper';
 // import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
 import StepLabel from '@mui/material/StepLabel';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import CardContent from '@mui/material/CardContent';
-import InputAdornment from '@mui/material/InputAdornment';
-import Gallery from './gallery';
 import axios from 'axios';
+import Gallery from './gallery';
 // ** Third Party Imports
-import * as yup from 'yup';
-import toast from 'react-hot-toast';
-import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { Controller, useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import * as yup from 'yup';
 // import DatePickerWrapper from 'styles/libs/react-datepicker';
 // ** Icon Imports
 import Icon from 'components/icon';
 import 'react-datepicker/dist/react-datepicker.css';
 // ** Custom Components Imports
-import StepperCustomDot from './StepperCustomDot';
 import { TextField as CustomTextField, TextField } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import StepperCustomDot from './StepperCustomDot';
 // ** Styled Components
-import StepperWrapper from 'styles/mui/stepper';
 import DatePicker from 'react-datepicker';
+import StepperWrapper from 'styles/mui/stepper';
 
 const steps = [
   {
@@ -358,12 +356,12 @@ const StepperLinearWithValidation = () => {
           // <DatePickerWrapper sx={{ '& .react-datepicker-wrapper': { width: 'auto' } }}>
           <form key={1} onSubmit={handlePersonalSubmit(onSubmit)}>
             <Grid container spacing={5}>
-              <Grid item xs={12}>
+              <Grid item xs={12} sx={{mt:3}}>
                 <Typography variant="body2" sx={{ fontWeight: 600, color: 'text.primary' }}>
-                  {steps[1].title}
+                  {steps[0].title}
                 </Typography>
                 <Typography variant="caption" component="p">
-                  {steps[1].subtitle}
+                  {steps[0].subtitle}
                 </Typography>
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -623,12 +621,12 @@ const StepperLinearWithValidation = () => {
         return (
           <form key={2} onSubmit={handleGallerySubmit(onSubmit)}>
             <Grid container spacing={5}>
-              <Grid item xs={12}>
+              <Grid item xs={12} sx={{mt:3}}>
                 <Typography variant="body2" sx={{ fontWeight: 600, color: 'text.primary' }}>
-                  {steps[2].title}
+                  {steps[1].title}
                 </Typography>
                 <Typography variant="caption" component="p">
-                  {steps[2].subtitle}
+                  {steps[1].subtitle}
                 </Typography>
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -711,7 +709,7 @@ const StepperLinearWithValidation = () => {
         return (
           <form key={2} onSubmit={handleSocialSubmit(onSubmit)}>
             <Grid container spacing={5}>
-              <Grid item xs={12}>
+              <Grid item xs={12} sx={{mt:3}}>
                 <Typography variant="body2" sx={{ fontWeight: 600, color: 'text.primary' }}>
                   {steps[2].title}
                 </Typography>
@@ -829,12 +827,12 @@ const StepperLinearWithValidation = () => {
         return (
           <form key={0} onSubmit={handleAccountSubmit(onSubmit)}>
             <Grid container spacing={5}>
-              <Grid item xs={12}>
+              <Grid item xs={12} sx={{mt:3}}>
                 <Typography variant="body2" sx={{ fontWeight: 600, color: 'text.primary' }}>
-                  {steps[0].title}
+                  {steps[3].title}
                 </Typography>
                 <Typography variant="caption" component="p">
-                  {steps[0].subtitle}
+                  {steps[3].subtitle}
                 </Typography>
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -1018,55 +1016,53 @@ const StepperLinearWithValidation = () => {
   };
 
   return (
-    <Card>
-      <CardContent>
-        <StepperWrapper>
-          <Stepper activeStep={activeStep}>
-            {steps.map((step, index) => {
-              const labelProps = {};
-              if (index === activeStep) {
+    <Grid>
+      <StepperWrapper >
+        <Stepper activeStep={activeStep}>
+          {steps.map((step, index) => {
+            const labelProps = {};
+            if (index === activeStep) {
+              labelProps.error = false;
+              if (
+                (accountErrors.email || accountErrors.username || accountErrors.password || accountErrors['confirm_password']) &&
+                activeStep === 3
+              ) {
+                labelProps.error = true;
+              } else if ((personalErrors['registered_date'] || personalErrors['first-name']) && activeStep === 0) {
+                labelProps.error = true;
+              } else if (galleryErrors.logo || (galleryErrors.gallery && activeStep === 1)) {
+                labelProps.error = true;
+              } else if (
+                (socialErrors.instagram || socialErrors.twitter || socialErrors.facebook || socialErrors.linkedIn) &&
+                activeStep === 2
+              ) {
+                labelProps.error = true;
+              } else {
                 labelProps.error = false;
-                if (
-                  (accountErrors.email || accountErrors.username || accountErrors.password || accountErrors['confirm_password']) &&
-                  activeStep === 3
-                ) {
-                  labelProps.error = true;
-                } else if ((personalErrors['registered_date'] || personalErrors['first-name']) && activeStep === 0) {
-                  labelProps.error = true;
-                } else if (galleryErrors.logo || (galleryErrors.gallery && activeStep === 1)) {
-                  labelProps.error = true;
-                } else if (
-                  (socialErrors.instagram || socialErrors.twitter || socialErrors.facebook || socialErrors.linkedIn) &&
-                  activeStep === 2
-                ) {
-                  labelProps.error = true;
-                } else {
-                  labelProps.error = false;
-                }
               }
+            }
 
-              return (
-                <Step key={index}>
-                  <StepLabel {...labelProps} StepIconComponent={StepperCustomDot}>
-                    <div className="step-label">
-                      <Typography className="step-number">{`0${index + 1}`}</Typography>
-                      <div>
-                        <Typography className="step-title">{step.title}</Typography>
-                        <Typography className="step-subtitle">{step.subtitle}</Typography>
-                      </div>
+            return (
+              <Step key={index} >
+                <StepLabel {...labelProps} StepIconComponent={StepperCustomDot}>
+                  <div className="step-label">
+                    <Typography className="step-number">{`0${index + 1}`}</Typography>
+                    <div>
+                      <Typography className="step-title">{step.title}</Typography>
+                      <Typography className="step-subtitle">{step.subtitle}</Typography>
                     </div>
-                  </StepLabel>
-                </Step>
-              );
-            })}
-          </Stepper>
-        </StepperWrapper>
-      </CardContent>
+                  </div>
+                </StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+      </StepperWrapper>
 
       <Divider sx={{ m: '0 !important' }} />
 
-      <CardContent>{renderContent()}</CardContent>
-    </Card>
+      <Grid>{renderContent()}</Grid>
+    </Grid>
   );
 };
 
