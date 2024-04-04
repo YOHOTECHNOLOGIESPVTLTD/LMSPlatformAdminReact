659173225c0fdfafaa0c9c1591c56516829b11d5
anish-yoho <anish.yoho@gmail.com>
2024-04-04 12:03:33 +0530

    commit anish

--- src/features/subscription-management/plans/components/plan-details.js
+++ src/features/subscription-management/plans/components/plan-details.js
@@ -3,7 +3,7 @@ import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-
+import { Grid } from '@mui/material';
 // ** Icon Imports
 import Icon from 'components/icon';
 
@@ -36,61 +36,61 @@ const PlanDetails = (props) => {
   const theme = useTheme();
   console.log( 'Plansxxx',plans);
   const renderFeatures = () => {
-   
       return (
-        <div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Grid>
+          <Grid sx={{ display: 'flex', alignItems: 'center',mb:1 }}>
             <span style={{ display: 'inline-flex', color: 'text.secondary', marginRight: '8px' }}>
               <Icon icon="tabler:circle" fontSize="0.875rem" />
             </span>
             <Typography style={{ color: 'text.secondary' }}>
-            Admins {plans?.features?.no_of_admins}
+            Admins <span style={{marginLeft:'15px'}}>{plans?.features?.no_of_admins}</span> 
             </Typography>
-          </div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
+          </Grid>
+          <Grid sx={{ display: 'flex', alignItems: 'center',mb:1 }}>
+            <span style={{ display: 'inline-flex', color: 'text.secondary', marginRight: '8px',mb:1 }}>
+              <Icon icon="tabler:circle" fontSize="0.875rem" />
+            </span>
+            <Typography style={{ color: 'text.secondary' }}>
+             Batches <span style={{marginLeft:'9px'}}>{plans?.features?.no_of_batches}</span> 
+            </Typography>
+          </Grid>
+          <Grid sx={{ display: 'flex', alignItems: 'center',mb:1 }}>
             <span style={{ display: 'inline-flex', color: 'text.secondary', marginRight: '8px' }}>
               <Icon icon="tabler:circle" fontSize="0.875rem" />
             </span>
             <Typography style={{ color: 'text.secondary' }}>
-             Batches {plans?.features?.no_of_batches}
+             Classes <span style={{marginLeft:'12px'}}>{plans?.features?.no_of_batches}</span>
             </Typography>
-          </div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
+          </Grid>
+          <Grid sx={{ display: 'flex', alignItems: 'center',mb:1 }}>
             <span style={{ display: 'inline-flex', color: 'text.secondary', marginRight: '8px' }}>
               <Icon icon="tabler:circle" fontSize="0.875rem" />
             </span>
             <Typography style={{ color: 'text.secondary' }}>
-             Classes {plans?.features?.no_of_batches}
+             Courses <span style={{marginLeft:'10px'}}>{plans?.features?.no_of_batches}</span>
             </Typography>
-          </div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
+          </Grid>
+          <Grid sx={{ display: 'flex', alignItems: 'center',mb:1 }}>
             <span style={{ display: 'inline-flex', color: 'text.secondary', marginRight: '8px' }}>
               <Icon icon="tabler:circle" fontSize="0.875rem" />
             </span>
             <Typography style={{ color: 'text.secondary' }}>
-             Courses {plans?.features?.no_of_batches}
+             Staffs <span style={{marginLeft:'28px'}}>{plans?.features?.no_of_batches}</span>
             </Typography>
-          </div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
+          </Grid>
+          <Grid sx={{ display: 'flex', alignItems: 'center',mb:1 }}>
             <span style={{ display: 'inline-flex', color: 'text.secondary', marginRight: '8px' }}>
               <Icon icon="tabler:circle" fontSize="0.875rem" />
             </span>
             <Typography style={{ color: 'text.secondary' }}>
-             Staffs {plans?.features?.no_of_batches}
+             Students <span style={{marginLeft:'5px'}}>{plans?.features?.no_of_batches}</span> 
             </Typography>
-          </div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <span style={{ display: 'inline-flex', color: 'text.secondary', marginRight: '8px' }}>
-              <Icon icon="tabler:circle" fontSize="0.875rem" />
-            </span>
-            <Typography style={{ color: 'text.secondary' }}>
-             Students {plans?.features?.no_of_batches}
-            </Typography>
-          </div>
-        </div>
+          </Grid>
+        </Grid>
       );
     }
 
+    const imgUrl = plans?.image ? plans?.image : "https://www.charitycomms.org.uk/wp-content/uploads/2019/02/placeholder-image-square.jpg";
 
   return (
     <BoxWrapper
@@ -101,6 +101,7 @@ const PlanDetails = (props) => {
     >
       <Box sx={{ mb: 4, display: 'flex', justifyContent: 'center' }}>
         {/* <img width={50} src={`${plans?.imgSrc}`} height={plans?.imgHeight} alt={`${plans?.title}-plan-img`} /> */}
+        <img src={imgUrl} alt='sub-img' height={100} style={{borderRadius:'0.5rem'}}/>
       </Box>
       <Box sx={{ textAlign: 'center' }}>
         <Typography sx={{ mb: 1.5, fontWeight: 500, lineHeight: 1.385, fontSize: '1.625rem' }}>{plans?.plan_name}</Typography>
@@ -117,8 +118,8 @@ const PlanDetails = (props) => {
         </Box>
       </Box>
       <BoxFeature>{renderFeatures()}</BoxFeature>
-      <Button fullWidth color={plans?.currentPlan ? 'success' : 'primary'} variant={plans?.popularPlan ? 'contained' : 'tonal'}>
-        {plans?.currentPlan ? 'Your Current Plan' : 'Upgrade'}
+      <Button fullWidth color='primary' variant={plans?.popularPlan ? 'contained' : 'tonal'}>
+        Edit Subscription
       </Button>
     </BoxWrapper>
   );
